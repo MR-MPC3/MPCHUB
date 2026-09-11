d7a3e247834b5f50d56ba25454eb1707e89b5195
MPC <nguyendangmigiang@gmail.com>
2026-09-11 15:22:11 +0700

    Update FatCaHub.lua

--- FatCaHub.lua
+++ FatCaHub.lua
@@ -83,7 +83,7 @@ local Window = Fluent:CreateWindow({
     Size = UDim2.fromOffset(580, 320),
     Acrylic = true,
     ToggleIcon = "rbxassetid://13717478897",
-    ToggleIconSize = UDim2.fromOffset(60, 60),
+    ToggleIconSize = UDim2.fromOffset(40, 40),
 })
 
 local TabDefinitions = {
