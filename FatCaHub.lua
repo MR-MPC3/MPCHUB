8a7841baa38a4dce49d5e354e663ec1681937cbf
MPC <nguyendangmigiang@gmail.com>
2026-09-11 11:59:24 +0700

    Update FatCaHub.lua

--- FatCaHub.lua
+++ FatCaHub.lua
@@ -83,7 +83,7 @@ local Window = Fluent:CreateWindow({
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true,
     Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl
+    MinimizeKey = Enum.KeyCode.End
 })
 
 local TabDefinitions = {
